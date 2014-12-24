65f555966f861dd3e284d325371fb75d9aa95c44
bmoren <ben@benmoren.com>
2014-12-23 23:45:39 -0600

    cleaning / setting up destroy function

--- sketch.js
+++ sketch.js
@@ -8,9 +8,9 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
 
-  for (var i=0; i<1; i++) {
+  for (var i=0; i<3; i++) {
 	  playerColor = color(random(255),random(255),random(255));
-	  players.push(new bike(1,3,5,playerColor));
+	  players.push(new bike(i,5,3,6,playerColor));
   }
 }
 
@@ -27,19 +27,20 @@ function draw() {
 
 }
 
-function bike(spd, dir, bikeSz, color){
+function bike(playerID, spd, dir, bikeSz, color){
  
  	this.color = color ;
 	this.x = round(random(width));
-  	this.y = round(random(height)); //use whole numbers so getting values from the pixel array later is easier!
-  	this.bikeSize = bikeSz;
-  	this.speed = spd;
-  	this.direction = dir ;
-  	//console.log("this.x: "+this.x+ " && this.y: "+this.y)
+	this.y = round(random(height)); //use whole numbers so getting values from the pixel array later is easier!
+	this.bikeSize = bikeSz;
+	this.speed = spd;
+	this.direction = dir ;
+  this.playerID = playerID;
+  
 
   this.move = function(up, down, left, right) {
 
-	if (keyIsPressed === true){ // determine direction //reset this using HFT when implemented
+	if (keyIsPressed === true){ // determine direction //reimplement this using HFT for the controller
     if (key === up){
       this.direction = 1 ;
     }
@@ -56,19 +57,19 @@ function bike(spd, dir, bikeSz, color){
 
     if (this.direction === 1){ //up
   		this.y -= this.speed ; 
-      this.dirDetectY = -1;
+      this.dirDetectY = -1; // -1Y = up
   	}
   	if (this.direction === 2){ //down
   		this.y += this.speed ; 
-      this.dirDetectY = 1;
+      this.dirDetectY = 1; // +1Y = down
   	}
   	if (this.direction === 3){ //left
   		this.x -= this.speed ; 
-      this.dirDetectX = -1;
+      this.dirDetectX = -1; //-1X = left
   	}
   	if (this.direction === 4){ //right
   		this.x += this.speed ; 
-      this.dirDetectX = 1;
+      this.dirDetectX = 1; //+1X = right
   	}
 
   };// close move class
@@ -78,33 +79,34 @@ function bike(spd, dir, bikeSz, color){
   	noStroke();
   	fill(this.color);
     rect(this.x, this.y, this.bikeSize, this.bikeSize);
-    fill(255,255,255);
   }; //close display
 
   this.collision = function(){
+    fill(255,255,255);
+
     // if(get(x,y) != color(0)){
     //   console.log("dead")
     // }
 
- fill(255,255,255);
-   //X
-  if(this.direction == 3 || this.direction == 4){
-   if(this.dirDetectX == 1){
-    rect(this.x+this.dirDetectX+10,this.y,1,1);
-   } else if(this.dirDetectX == -1) {
-    rect(this.x+this.dirDetectX-10,this.y,1,1);
-   }
- }
+    //****** Need to figure out scalable offset
+    var szOffset = 2; //2 is about good for a bikesize of 6
+    //X
+    if(this.direction == 3 || this.direction == 4){
+      if(this.dirDetectX == 1){
+        rect(this.x+this.dirDetectX+szOffset,this.y,1,1);
+      } else if(this.dirDetectX == -1) {
+        rect(this.x+this.dirDetectX-szOffset,this.y,1,1);
+      }
+    }// close X
 
-   if(this.direction == 1 || this.direction == 2){
-     if(this.dirDetectY == 1){
-      console.log("foo")
-      rect(this.x,this.y+this.dirDetectY+10,1,1);
-     } else if(this.dirDetectY == -1){
-      rect(this.x,this.y+this.dirDetectY-10,1,1);
-
-     }
-   }
+    //Y
+    if(this.direction == 1 || this.direction == 2){
+      if(this.dirDetectY == 1){
+        rect(this.x,this.y+this.dirDetectY+szOffset,1,1);
+      } else if(this.dirDetectY == -1){
+        rect(this.x,this.y+this.dirDetectY-szOffset,1,1);
+      }
+    } //close Y
 
   }; //close collision
 
@@ -116,8 +118,7 @@ function bike(spd, dir, bikeSz, color){
   			this.x = 0; //loop to left side
   		}
   		if (mode === 'destroy' || mode === 'vertLoop'){
-  			//destroy code here
-  			console.log('destroyd, right');
+  			this.destroy();
   		}
   	} 
 
@@ -126,8 +127,7 @@ function bike(spd, dir, bikeSz, color){
   			this.x = width; //loop to right side
   		}
   		if (mode === 'destroy' || mode === 'vertLoop'){
-  			//destroy code here
-  			console.log('destroyd, left');
+  			this.destroy();
   		}
 	}
 
@@ -136,8 +136,7 @@ function bike(spd, dir, bikeSz, color){
   			this.y = 0; //loop to top
   		}
   		if (mode === 'destroy' || mode === 'horizLoop'){
-  			//destroy code here
-  			console.log('destroyd, bottom');
+        this.destroy();
   		}
   	}
 
@@ -146,13 +145,17 @@ function bike(spd, dir, bikeSz, color){
   			this.y = height; //loop to bottom
   		}
   		if (mode === 'destroy' || mode === 'horizLoop'){
-  			//destroy code here
-  			console.log('destroyd, top');
+  			this.destroy();
   		}
   	}
-
-
   } //close edge detection 
+
+  this.destroy = function(){
+    
+    console.log("destroy fired" + this.playerID);
+
+  }
+
 
 } // close Bike
 
